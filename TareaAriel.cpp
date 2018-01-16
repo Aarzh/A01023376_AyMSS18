fe2eea8bb0d1568d185235e06b436d6efd40cc55
Aarzh <aaronzajac97@gmail.com>
2018-01-16 13:50:06 -0600

    Tarea1

---
+++ TareaAriel.cpp
@@ -0,0 +1,16 @@
+/*
+    Aaron Zajac
+    A01023376
+*/
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <sstream>
+#include "Tarea1.h"
+
+using namespace std;
+
+int main()
+{
+    Hora h;
+}
